23cbda93f4d0091eb9270d7e9449ccc622b9a790
amperkean <amper_kean@yahoo.com>
2017-01-27 12:09:04 +0800

    relocated db

--- petsure-web-api/Models/PetSureModel.Designer.cs
+++ petsure-web-api/Models/PetSureModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\kean\documents\visual studio 2015\Projects\petsure-web-api\petsure-web-api\Models\PetSureModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\kean\Documents\Visual Studio 2015\Projects\petsure-web-api\petsure-web-api\Models\PetSureModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
